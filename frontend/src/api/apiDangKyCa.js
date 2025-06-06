2c04e066966b71d2cf3afee50190449163219eff
datmino09 <datmino93737@gmail.com>
2025-06-06 12:37:52 +0700

    api delete DKC

--- frontend/src/api/apiDangKyCa.js
+++ frontend/src/api/apiDangKyCa.js
@@ -17,3 +17,17 @@ export const fetchDKCByNhanVien = async (MaNV,NgayDangKy) =>{
       return { success: false, message: "Lỗi kết nối đến server" };
     }
 }
+export const deleteDangKyCa = async (MaDKC) =>{
+  try {
+    const response = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/dangkyca/${MaDKC}`);
+    return {success: true, data: response.data.message};
+  } catch (error) {
+    if (error.response?.status === 404) {
+      return { success: false, message: "Đăng ký ca làm không tồn tại" };
+    }else if(error.response.status === 400){
+      return { success: false, message: "Không thể xóa đăng ký ca làm khi đã có chấm công" };
+    }
+    console.error("Lỗi xóa ĐK Ca:", error);
+    return { success: false, message: "Lỗi kết nối đến server" };
+  }
+} 
