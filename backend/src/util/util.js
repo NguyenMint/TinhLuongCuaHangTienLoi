56bc300927f18ed964588563f7408673a9a5855f
datmino09 <datmino93737@gmail.com>
2025-06-11 15:47:13 +0700

    api create bang luong

--- backend/src/util/util.js
+++ backend/src/util/util.js
@@ -10,4 +10,10 @@ function tinhTongGioLamCaLam(thoiGianBatDau, thoiGianKetThuc) {
   }
   return (ketThuc - batDau) / 3600000;
 }
-module.exports = { getSoNgayTrongThang, tinhTongGioLamCaLam };
+const formatDate = (date) => {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+module.exports = { getSoNgayTrongThang, tinhTongGioLamCaLam, formatDate };
