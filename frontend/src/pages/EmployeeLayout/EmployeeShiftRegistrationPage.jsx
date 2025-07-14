14ff7d36c0073f7c56ee4ae0637249a308f8efbd
NguyenMint <truongnm2406@gmail.com>
2025-07-14 23:40:31 +0700

    fix bug

--- frontend/src/pages/EmployeeLayout/EmployeeShiftRegistrationPage.jsx
+++ frontend/src/pages/EmployeeLayout/EmployeeShiftRegistrationPage.jsx
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from "react";
 import { ShiftRegistration } from "../../components/Employee/ShiftRegistration";
 import { ControlRegistrationModal } from "../../components/Employee/ControlRegistrationModal";
 import { fetchCaLam } from "../../api/apiCaLam";
-import { getAllLLVByNhanVien, dangKyCa, huyDangKyCa } from "../../api/apiLichLamViec";
-import { format } from "date-fns";
+import { format, isBefore, startOfDay } from "date-fns";
+
+import {
+  getAllLLVByNhanVien,
+  dangKyCa,
+  huyDangKyCa,
+} from "../../api/apiLichLamViec";
 export const EmployeeShiftRegistrationPage = () => {
   const currentDate = new Date();
   const user = JSON.parse(localStorage.getItem("user"));
@@ -33,6 +38,13 @@ export const EmployeeShiftRegistrationPage = () => {
   }, []);
   const handleDangKyCa = async (MaCa, day) => {
     const NgayLam = format(day, "yyyy-MM-dd");
+    const today = startOfDay(new Date());
+    const selectedDate = startOfDay(new Date(day));
+
+    if (isBefore(selectedDate, today)) {
+      alert("Không thể thêm ca làm việc cho ngày đã qua.");
+      return;
+    }
     const formData = {
       MaTK: user.MaTK,
       MaCaLam: MaCa,
@@ -46,17 +58,17 @@ export const EmployeeShiftRegistrationPage = () => {
     alert("Đăng ký ca thành công");
     fetchLichLamViecByNhanVien();
   };
-  const handleHuyDangKy = async () =>{
+  const handleHuyDangKy = async () => {
     const response = await huyDangKyCa(selectedLLV.MaLLV);
-    if(!response.success){
-        alert(response.message || "Hủy đăng ký ca thất bạt");
-        setShowControl(false);
-        return;
+    if (!response.success) {
+      alert(response.message || "Hủy đăng ký ca thất bạt");
+      setShowControl(false);
+      return;
     }
     alert("Hủy ca thành công");
     setShowControl(false);
     fetchLichLamViecByNhanVien();
-  }
+  };
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto px-4 py-6">
